916ae16780940a6ea4130e169fbd005024a8a8e1
ScottAY04 <auyeungscott@gmail.com>
2026-02-05 15:40:53 -0500

    added json methods

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,2 +1,20 @@
 const users = {};
 
+//a function that responds with a json object
+const respondJSON = (request, response, status, object) => {
+    const content = JSON.stringify(object);
+
+    //header related stuff
+    const headers = {
+        "Content-Type": 'application/json',
+        "Content-Length": Buffer.byteLength(content, 'utf8');
+    };
+
+    response.writeHead(status, headers);
+
+    if(response.method !== 'HEAD'){ 
+        response.writeHead(content);
+    }
+
+    response.end();
+}
